bfc5b89acbb46204d92e275161f06fb78b877b06
enrico baiano <s309484@studenti.polito.it>
2025-03-30 12:28:58 +0200

    modicfiche

--- complex_number.hpp
+++ complex_number.hpp
@@ -10,27 +10,40 @@ private:
 
 public:
     
-    complex_number() : m_re(0), m_im(0) {}
+    complex_number() : 
+		m_re(0), m_im(0) 
+		{
+		
+		}
     
-    /* Costruttore user-defined */
-    complex_number(T re, T im) : m_re(re), m_im(im) {
-        
-    }
+    // Costructor  user-defined //
+    complex_number(T re, T im) : 
+		m_re(re), m_im(im) 
+		{
+			
+		}
 
-    /* Restituisce perte reale */
-    T re() const { return m_re; }
+    // compute real part //
+    T re() const 
+		{ 
+		return m_re; 
+		}
     
-    /* Restituisce parte immaginaria */
-    T im() const { return m_im; }
+    //compute immaginary part//
+    T im() const 
+		{
+		return m_im; 
+		}
     
-    /* Operatore += */
-    complex_number& operator+=(const complex_number& other) {
+    // += //
+    complex_number& operator+=(const complex_number& other) 
+	{
         m_re = m_re + other.m_re ;
         m_im = m_im + other.m_im;
         return *this;
     }
     
-    /* Operatore + */
+    // + //
     complex_number operator+(const complex_number& other) const {
         complex_number result_on_first = *this; 
         result_on_first += other;
@@ -43,8 +56,9 @@ public:
 	
 	
 	
-	/* Operatore *= */
-    complex_number& operator*=(const complex_number& other) {
+	// *= //
+    complex_number& operator*=(const complex_number& other) 
+	{
         T new_re = m_re*other.m_re-m_im*other.m_im;
         T new_im = other.m_im*m_re +m_im*other.m_re;
         m_re=new_re;
@@ -52,15 +66,17 @@ public:
         return *this;
     }
     
-    /* Operatore * */
-    complex_number operator*(const complex_number& other) const {
+    // Operatore * //
+    complex_number operator*(const complex_number& other) const 
+	{
         complex_number result = *this;
         result *= other;
         return result;
     }
     
-	/* Operatore coniugato */
-	complex_number coniugate() const {
+	// Operatore coniugato //
+	complex_number coniugate() const 
+	{
         return complex_number(m_re, -m_im);
     }
 };
